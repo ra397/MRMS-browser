307d3354b13d533bb2d603f72a43a0cf51e764c2
Alaya <ralaya@uiowa.edu>
2025-11-07 11:09:31 -0600

    updated customOverlay.js to Radek's new implementation

--- src/display/customOverlay.js
+++ src/display/customOverlay.js
@@ -1,157 +1,225 @@
-(function () {
+export class TiledRasterLayer {
+    constructor(map, name, urlTemplate, options = {}) {
+        this.layer = null;
+        this.map = map;
+        this.name = name || 'Custom Tile Layer';
+        this.urlTemplate = urlTemplate;           // URL template with placeholders for zoom (z), x, and y tile coordinates.
+                                                  // Example: '/tiles/{z}/{x}/{y}.png'
+        // Optional settings
+        this.tms_style  = options?.tms_style || false;
+        this.tileSize = options?.tileSize || new google.maps.Size(256, 256);
+        this.minZoom = options?.minZoom || 5;
+        this.maxZoom = options?.maxZoom || 18;
+        this.divClass = options?.divClass || null;
+        this.imgClass = options?.divClass || null;
+    }
+    getURL = (e, z) => {
+        const zxy = {
+            "z": z,
+            "x": e.x,
+            "y": this.tms_style ? (1 << z) - e.y - 1 : e.y
+        };
+        return this.urlTemplate.replace(/\{(\w+)\}/g, (_, key) => zxy[key] ?? '');
+    }
+    #createLayer() {
+        return new google.maps.ImageMapType({
+            getTileUrl: this.getURL,
+            tileSize:   this.tileSize,
+            minZoom:    this.minZoom,
+            maxZoom:    this.maxZoom,
+            name:       this.name
+        });
+    }
 
-    function createClass(name, rules) {
+    isVisible() {
+        let visible = false;
+        this.map.overlayMapTypes.forEach((layer) => {
+            if (layer && layer.name === this.name) {
+                visible = true;
+            }
+        });
+        return visible;
+    }
+
+    show() {
+        this.hide();
+        this.layer = this.#createLayer();
+        this.map.overlayMapTypes.insertAt(0, this.layer);
+    }
+
+    hide() {
+        this.map.overlayMapTypes.forEach((layer, i) => {
+            if (layer.name === this.name) {
+                this.map.overlayMapTypes.removeAt(i);
+            }
+        });
+    };
+
+    setMap  = (map) => !map ? this.hide() : this.show();
+
+    destroy() {
+        this.setMap(null);
+        this.unbindAll();
+        this.urlTemplate = null
+        // Optional settings
+        this.tms_style  = null;
+        this.tileSize = null;
+        this.minZoom = null;
+        this.maxZoom = null;
+    }
+
+    update(urlTemplate) {
+        if (urlTemplate) this.urlTemplate = urlTemplate;
+        this.show();
+    }
+
+    setSource = this.update
+
+    setOpacity = (val) => this.layer.setOptions({ opacity: val})
+
+}
+
+const cssMercatorOverlay = [
+    {'.overlayDiv': "position: absolute; border-style: none; border-width: 0px; image-rendering: pixelated;"},
+    {'.overlayImg': "position: absolute; width: 100%; height: 100%;"},
+    {'.imgSmooth':  "image-rendering: auto;"}
+];
+
+///  use for image created in WGS84
+export class GeoOverlay extends google.maps.GroundOverlay {
+    constructor(image, bounds, map) {
+        super(image, bounds);
+        this.setMap(map);
+    }
+    remove() {
+        this.setMap(null);
+    }
+    destroy() {
+        this.setMap(null);
+        this.unbindAll();
+        this.image = null;
+        this.bounds = null;
+        this.set('url', null);
+    }
+
+    update(src) {
+        this.set('url', src);
+        this.setMap(this.getMap());
+    }
+    setSource = this.update
+}
+
+///  use for image created in WebMercator
+export class MercatorOverlay extends google.maps.OverlayView {
+    static cssInjected = false;
+
+    #bounds;
+    #div;
+    #img;
+    #pane;
+    #map;
+
+    constructor(image, bounds, map, smooth = false, pane = 'overlayLayer') {
+        super();
+
+        if (!MercatorOverlay.cssInjected && cssMercatorOverlay) {
+            cssMercatorOverlay.forEach(obj => {
+                const [selector, rules] = Object.entries(obj)[0];
+                this.#injectCSS(selector, rules);
+            });
+            MercatorOverlay.cssInjected = true;
+        }
+
+        this.#bounds = bounds;
+        this.#pane = pane;
+        this.#map = map;
+
+        this.#div = document.createElement('div');
+        this.#div.className = 'overlayDiv';
+
+        this.#img = document.createElement('img');
+        this.#img.className = 'overlayImg';
+        if (smooth) this.#img.classList.add('imgSmooth');
+        this.#img.src = image;
+
+        this.#div.appendChild(this.#img);
+        this.setMap(map);
+    }
+
+    #injectCSS(selector, rules) {
         const style = document.createElement('style');
         style.type = 'text/css';
-        document.getElementsByTagName('head')[0].appendChild(style);
-        if (!(style.sheet || {}).insertRule)
-            (style.styleSheet || style.sheet).addRule(name, rules);
-        else
-            style.sheet.insertRule(name + "{" + rules + "}", 0);
+        document.head.appendChild(style);
+        if (!(style.sheet || {}).insertRule) {
+            (style.styleSheet || style.sheet).addRule(selector, rules);
+        } else {
+            style.sheet.insertRule(`${selector}{${rules}}`, 0);
+        }
     }
 
-    createClass('.overlayDiv', "position: absolute; border-style: none; border-width: 0px; image-rendering: pixelated;");
-    createClass('.overlayImg', "position: absolute; width: 100%; height: 100%;");
-
-
-    function initOverlays() {
-        // ---- GroundOverlay wrapper (don’t subclass GroundOverlay directly) ----
-        function geoOverlay(image, bounds, map) {
-            // Normalize bounds if passed as { sw, ne }
-            if (
-                !(bounds instanceof google.maps.LatLngBounds) &&
-                bounds && 'sw' in bounds && 'ne' in bounds
-            ) {
-                bounds = new google.maps.LatLngBounds(bounds.sw, bounds.ne);
-            }
-
-            const overlay = new google.maps.GroundOverlay(image, bounds);
-            overlay.setMap(map);
-
-            overlay.remove = function () {
-                overlay.setMap(null);
-            };
-
-            overlay.setSource = function (src) {
-                // GroundOverlay reads the 'url' property; setting & re-adding refreshes drawing
-                overlay.set('url', src);
-                overlay.setMap(map);
-            };
-
-            overlay.animate = overlay.setSource;
-
-            return overlay;
-        }
-
-        function MercatorOverlay(image, bounds, map) {
-            // Store state
-            this._bounds_ = bounds;
-            this._div_ = document.createElement('div');
-            this._div_.className = 'overlayDiv';
-
-            this._img_ = new Image();
-            this._img_.className = 'overlayImg';
-            this._img_.src = image;
-            this._div_.appendChild(this._img_);
-
-            this.style = this._div_.style;
-
-            // Optional: cache after first measure
-            this.naturalHeight = undefined;
-            this.naturalWidth = undefined;
-
-            // Normalize bounds if passed as { sw, ne }
-            if (
-                !(this._bounds_ instanceof google.maps.LatLngBounds) &&
-                this._bounds_ && 'sw' in this._bounds_ && 'ne' in this._bounds_
-            ) {
-                this._bounds_ = new google.maps.LatLngBounds(this._bounds_.sw, this._bounds_.ne);
-            }
-
-            // Attach to map
-            this.setMap(map);
-        }
-
-        // Proper inheritance: AFTER Google is available
-        MercatorOverlay.prototype = Object.create(google.maps.OverlayView.prototype);
-        MercatorOverlay.prototype.constructor = MercatorOverlay;
-
-        MercatorOverlay.prototype.onAdd = function () {
-            const panes = this.getPanes();
-            panes.overlayLayer.appendChild(this._div_);
-            // Cache projection
-            this.projection = this.getProjection();
-        };
-
-        MercatorOverlay.prototype.draw = function () {
-            const sw = this.projection.fromLatLngToDivPixel(this._bounds_.getSouthWest());
-            const ne = this.projection.fromLatLngToDivPixel(this._bounds_.getNorthEast());
-
-            this.style.left = sw.x + 'px';
-            this.style.top = ne.y + 'px';
-            this.style.width = (ne.x - sw.x) + 'px';
-            this.style.height = (sw.y - ne.y) + 'px';
-        };
-
-        MercatorOverlay.prototype.onRemove = function () {
-            if (this._div_ && this._div_.parentNode) {
-                this._div_.parentNode.removeChild(this._div_);
-            }
-            this._div_ = null;
-        };
-
-        MercatorOverlay.prototype.remove = function () {
-            this.setMap(null);
-        };
-
-        MercatorOverlay.prototype.setOpacity = function (opacity) {
-            this.style.opacity = opacity;
-        };
-
-        MercatorOverlay.prototype.setSource = function (src) {
-            this._img_.src = src;
-        };
-
-        MercatorOverlay.prototype.animate = MercatorOverlay.prototype.setSource;
-
-        MercatorOverlay.prototype.fromLatLngToColRow = function (_latLng) {
-            if (!this._bounds_.contains(_latLng)) return [null, null];
-
-            // Ensure natural sizes are available (image may not be loaded instantly)
-            const nW = this._img_.naturalWidth, nH = this._img_.naturalHeight;
-            this.naturalHeight = nH;
-            this.naturalWidth = nW;
-
-            const oL = this._div_.offsetLeft, oT = this._div_.offsetTop;
-            const oW = this._div_.offsetWidth, oH = this._div_.offsetHeight;
-            const xy = this.projection.fromLatLngToDivPixel(_latLng);
-
-            const rW = nW / oW, rH = nH / oH;
-            const col = Math.floor(Math.abs(oL - xy.x) * rW);
-            const row = Math.floor(Math.abs(oT - xy.y) * rH);
-            return [col, row];
-        };
-
-        function customOverlay(imgSrc, bounds, map, overlayType) {
-            if (overlayType === 'OverlayView') {
-                return new MercatorOverlay(imgSrc, bounds, map);
-            } else if (overlayType === 'GroundOverlay') {
-                return geoOverlay(imgSrc, bounds, map);
-            }
-            return false;
-        }
-
-
-        window.geoOverlay = geoOverlay;
-        window.mercatorOverlay = MercatorOverlay;
-        window.CustomOverlay = customOverlay;
+    onAdd() {
+        this.getPanes()[this.#pane].appendChild(this.#div);
     }
 
-    // Initialize now or wait for Google
-    if (window.google && window.google.maps) {
-        initOverlays();
-    } else {
-        // Wait for the signal from the Google Maps callback in your HTML
-        window.addEventListener('gmaps-loaded', initOverlays, { once: true });
+    draw() {
+        const projection = this.getProjection();
+        if (!projection) return;
+
+        const sw = projection.fromLatLngToDivPixel(this.#bounds.getSouthWest());
+        const ne = projection.fromLatLngToDivPixel(this.#bounds.getNorthEast());
+
+        Object.assign(this.#div.style, {
+            left: `${sw.x}px`,
+            top: `${ne.y}px`,
+            width: `${ne.x - sw.x}px`,
+            height: `${sw.y - ne.y}px`
+        });
     }
-})();
+
+    onRemove() {
+        if (this.#div.parentNode) this.#div.parentNode.removeChild(this.#div);
+        this.#div = null;
+    }
+
+    remove() {
+        this.setMap(null);
+    }
+
+    setOpacity(opacity) {
+        this.#img.style.opacity = opacity;
+    }
+
+    update(src) {
+        this.#img.src = src;
+    }
+
+    setSource = this.update;
+
+    show() {
+        this.setMap(this.#map);
+    }
+
+    hide() {
+        this.setMap(null);
+    }
+
+    destroy() {
+        this.remove();
+        this.unbindAll();
+        this.#div = null;
+        this.#img = null;
+        this.#bounds = null;
+        this.#map = null;
+    }
+
+}
+
+export function customOverlay(image_url, bounds, map, type = 'OverlayView', smooth = false, pane='overlayLayer') {
+    if (typeof bounds.extend !== 'function' && bounds.sw && bounds.ne) {
+        bounds = new google.maps.LatLngBounds(bounds.sw, bounds.ne);
+    }
+
+    return type === 'OverlayView'
+        ? new MercatorOverlay(image_url, bounds, map, smooth, pane)
+        : new GeoOverlay(image, bounds, map);
+}
--- src/display/display.js
+++ src/display/display.js
@@ -1,5 +1,5 @@
 import { RasterGenerator } from "./rasterGenerator.js";
-import '../display/customOverlay.js';
+import { customOverlay } from "./customOverlay.js";
 
 /*
 * Per request fileDataMap
@@ -17,10 +17,10 @@ document.addEventListener('display-matrices', async event => {
         requestFileDataMap[files[i]] = matrices[i];
     }
 
-    // for (const matrix of matrices) {
-    //     const raster = new RasterGenerator(matrix, LUT.ncols, LUT.nrows);
-    //     const img = await raster.generateUrl();
-    //     console.log(img);
-    //     const overlay = new CustomOverlay(img, LUT.bbox, map, 'OverlayView', 1);
-    // }
+    for (const matrix of matrices) {
+        const raster = new RasterGenerator(matrix, LUT.ncols, LUT.nrows);
+        const img = await raster.generateUrl();
+        console.log(img);
+        const overlay = customOverlay(img, LUT.bbox, map, 'OverlayView', 1);
+    }
 });
